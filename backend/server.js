6678359f0b8d0f6a74f716a73cdd64da04d2d9de
M Carmen Gonzalez Santacana <mcarmengonzalezsantacana@MacBook-Air-de-M.local>
2024-05-02 22:44:42 +0200

    backend

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
+
+dotenv.config();
+
+const __dirname = path.resolve();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+server.listen(PORT, () => {
+  connectToMongoDB(process.env.MONGO_DB_URI); // Pasar el URI de conexión de MongoDB como argumento
+  console.log(`Server Running on port ${PORT}`);
+});
